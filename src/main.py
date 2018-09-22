17af784583620f038d5a987788a985c73512901e
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-09-22 23:48:39 +1200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,27 @@
+class Brute():
+
+	def __init__(self):
+		self.best_value = 0
+
+	def solve(self, n, current_weight, current_value, weights, values, max_weight):
+		if n == 0 and current_weight <= max_weight and current_value > self.best_value:
+			self.best_value = current_value
+
+		if n == 0:
+			return
+
+		self.solve(n-1, current_weight, current_value, weights, values, max_weight)
+
+		self.solve(n-1, current_weight + weights[n], current_value + values[n], weights, values, max_weight)
+
+# runs the algorithm
+if __name__ == '__main__':
+	values = [60, 100, 120]
+	weights = [10, 20, 30]
+	max_weight = 50
+	n = len(values)
+
+	b = Brute()
+	b.solve(n - 1, 0, 0, weights, values, max_weight)
+
+	print(b.best_value)
